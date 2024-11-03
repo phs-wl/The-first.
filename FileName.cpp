c322e0792f74e4a80e9fba409f28f4531be34bde
破坏神 <phs920@qq.com>
2024-11-03 17:58:49 +0800

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -1,18 +1,12 @@
 #include<iostream>
-#include<algorithm>
-using namespace  std;
-int  main()
+using namespace std;
+int main()
 {
-    int a[9];
-    int i;
-    for (i = 0;i < 9;i++) {
-        cin >> a[i];
-    }
-    sort(a, a + 9);
-        for (i = 0;i < 9;i++) {
-            cout << a[i] << "";
-        }
-    cout << endl;
-    return 0;
+	int a, b, c;
+	cin >> a >> b >> c;
+	int max = a;
+	if (max < b) max = b;
+	if (max < c) max = c;
+	cout << "最大的数是" << max << endl;
+	return 0;
 }
-
