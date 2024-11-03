80aab35574feed51e347dcc0aaae5e27c8531892
破坏神 <phs920@qq.com>
2024-11-03 18:02:41 +0800

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -2,11 +2,27 @@
 using namespace std;
 int main()
 {
-	int a, b, c;
-	cin >> a >> b >> c;
-	int max = a;
-	if (max < b) max = b;
-	if (max < c) max = c;
-	cout << "最大的数是" << max << endl;
+	int score;
+	cin >> score;
+	if (0 <= score && score <= 100) {
+		if (90 <= score) {
+			cout << "A" << endl;
+		}
+		else if (80 <= score && score < 90) {
+			cout << "B" << endl;
+		}
+		else if (70 <= score && score < 80) {
+			cout << "C" << endl;
+		}
+		else if (60 <= score && score < 70) {
+			cout << "D" << endl;
+		}
+		else {
+			cout << "E" << endl;
+		}
+	}
+	else {
+		cout << "ERROR!" << endl;
+	}
 	return 0;
 }
